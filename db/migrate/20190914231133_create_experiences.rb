cc404cdd355eadc90285d83a84d402a08680a372
tpope19 <40743479+tpope19@users.noreply.github.com>
2019-09-15 19:53:57 -0400

    deleting files to redo

--- db/migrate/20190914231133_create_experiences.rb
+++
@@ -1,8 +0,0 @@
-class CreateExperiences < ActiveRecord::Migration[6.0]
-  def change
-    create_table :experiences do |t|
-      
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20190914231241_create_concerts.rb
+++
@@ -1,8 +0,0 @@
-class CreateConcerts < ActiveRecord::Migration[6.0]
-  def change
-    create_table :concerts do |t|
-      
-      t.timestamps
-    end
-  end
-end
