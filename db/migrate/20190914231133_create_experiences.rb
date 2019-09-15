345ffc4242fbe9e5ac02b42ec91410c4aac33d91
Thomas_Pope <tpope0928@gmail.com>
2019-09-15 18:48:59 -0400

    confused

--- db/migrate/20190914231133_create_experiences.rb
+++ db/migrate/20190914231133_create_experiences.rb
@@ -1,12 +1,7 @@
 class CreateExperiences < ActiveRecord::Migration[6.0]
   def change
     create_table :experiences do |t|
-      t.belongs_to :user, index:true
-      t.integer :rating
-      t.string  :comment
-      t.integer :artist_id
-      t.integer :concert_id
-    
+      
       t.timestamps
     end
   end
--- db/migrate/20190914231355_create_artists.rb
+++ db/migrate/20190914231355_create_artists.rb
@@ -1,9 +1,7 @@
 class CreateArtists < ActiveRecord::Migration[6.0]
   def change
     create_table :artists do |t|
-      t.string :name
-      t.integer :experiences_id
-      t.integer :concert_id
+
 
       t.timestamps
     end
