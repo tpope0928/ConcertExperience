639ffe10f7364450b806b03e3dbad41dbbf4a532
Thomas_Pope <tpope0928@gmail.com>
2019-09-17 19:12:56 -0400

    concerts controller logic

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,2 +1,34 @@
 class ConcertsController < ApplicationController
+    #books = concert
+
+    def index
+        if params[:user_id]
+            @concerts = User.find(params[:user_id]).concerts
+            flash.now[:danger] = "You haven't added any concerts yet!" if @concerts.empty?
+        end
+    end
+
+    def new
+        @concert = Concert.new
+        @concert.review.build
+    end
+
+    def create
+        @concert = Concert.create(concert_params)
+        @book.experiences.last.user = current_user
+        if @concert.save
+            redirect_to @concert
+        end
+    end
+
+    def show
+        @concert = Concert.find(params[:id])
+    end
+
+    private
+
+    def concert_params
+        params.require(:concert).permit(:city, :state, :venue, :date)
+    end
+    
 end
