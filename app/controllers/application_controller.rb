99c170150f335cd8d4ce27c29bd4f12e01dc428c
tpope19 <40743479+tpope19@users.noreply.github.com>
2019-09-19 17:54:14 -0400

    home page work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+    helper_method :current_user, :logged_in?
 
     def current_user
-        @current_user ||= User.find(session[:user_id])
+      @current_user ||= User.find_by_id(session[:user])
     end
 
-end
+     def logged_in?
+       current_user != nil
+     end
+  end
