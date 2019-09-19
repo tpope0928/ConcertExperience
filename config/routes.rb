c8dcf7e6208e4ddfadd4a76f7d00d0a0e153e138
tpope19 <40743479+tpope19@users.noreply.github.com>
2019-09-19 17:33:07 -0400

    initial config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  resources :sessions
+  
+  root 'users#new'
+
   resources :artists
   resources :concerts
   resources :experiences
-  resources :users
+  resources :users, only: [:new, :create, :destroy, :show, :index]
+
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
