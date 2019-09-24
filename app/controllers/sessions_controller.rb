6f89584c2d6d441c064302fe81ee1250f423b415
tpope19 <40743479+tpope19@users.noreply.github.com>
2019-09-24 19:35:24 -0400

    added login and logout functionality. 49:45

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+
+    def destroy
+        session.clear
+        redirect_to root_path
+      end
+    
+      def create
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+          session[:user_id] = user.id
+          redirect_to user_path(user)
+        else
+          flash[:message] = "Incorrect login info, please try again"
+          redirect_to "/login"
+        end
+      end
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  root "sessions#home"
   
-  get '/signup' =>  'users#new'
+  get '/signup'  =>  'users#new'
   post '/signup' => 'users#create'
+
+  #login route
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
+  #logout route
+  delete '/logout' => 'sessions#destroy'
+
 
 
   resources :artists
