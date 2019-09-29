56ffb598ab64303f10f5bd8e7da711cbd792ccee
Thomas_Pope <tpope0928@gmail.com>
2019-09-29 15:28:31 -0400

    experiences controller

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,63 +1,68 @@
 class ExperiencesController < ApplicationController
     #experiences = review
-
-    before_action :find_experience, only: [:show, :edit, :update, :destroy]
-    before_action :update_experience, only: [:edit]
-
-    def index
-        @experiences = Experience.all
-    end
-
-    def show
-    end
-
-    def new
-        @experience = Experience.new
-    end
-
-    def edit
-    end
-
-    def create
-        @experience = Experience.create(experience_params)
-        @experience.user_id = current_user.id
-        if @experience.save
-            redirect_to @experience
-        else
-            render :new
-        end
-    end
-
-    def update
-        if @experience.update(experience_params)
-          redirect_to @experience
-        else
-          render :edit
-        end
-    end
-
-    def destroy
-        @experience.destroy
-        flash[:danger] "Experience Deleted"
-        redirect_to user_path(current_user.id)
-    end
-
-    private
-
-    def experience_params
-        params.require(:experience).permit(:comment, :rating, :user_id, :concert_id)
-    end
-
-    def find_review
-        @review = Review.find(params[:id])
-    end
-  
-    def update_experience
-        unless logged_in? && current_user[:id] == @experience.user_id
-            flash[:danger] = "You cannot edit this concert experience"
-            redirect_to user_path
-        end
-    end
-
+    before_action :redirect_if_not_logged_in
+    before_action :set_experience, only: [:show, :edit, :update]
+    before_action :redirect_if_not_experience_author, only: [:edit, :update]
+ 
+   def index
+     if params[:concert_id] && @concert = Concert.find_by_id(params[:concert_id])
+        @experiences = @concert.experience
+     else
+       @error = "That post doesn't exist" if params[:post_id]
+       @experiences = Experience.all
+     end
+   end
+ 
+   def new
+     
+     if params[:concert_id] && @concert = Concert.find_by_id(params[:concert_id])
+       @experience = @concert.experience.build
+     else
+       @error = "That post doesn't exist" if params[:concert_id]
+       @experience = Experience.new
+     end
+   end
+ 
+   def create
+     @experience = current_user.experience.build(experience_params)
+     if @experience.save
+       redirect_to experiences_path
+     else
+       render :new
+     end
+   end
+ 
+ 
+   def show
+   end
+ 
+   def edit
+   end
+ 
+   def update
+     if @experience.update(experience_params)
+       redirect_to experience_path(@experience)
+     else
+       render :edit
+     end
+   end
+ 
+   private
+ 
+   def experience_params
+     params.require(:experience).permit(:comment, :rating, :concert_id, :user_id)
+   end
+ 
+   def set_experience
+     @experience = Experience.find_by(id: params[:id])
+     if !@experience
+       flash[:message] = "Experience was not found"
+       redirect_to experiences_path
+     end
+   end
+ 
+   def redirect_if_not_experience_author
+      redirect_to experiences_path if @experience.user != current_user
+   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,13 @@ Rails.application.routes.draw do
 
 
   resources :artists
-  resources :concerts
+  resources :concerts do
+    resources :experiences, only: [:new, :create, :index]
+  end
   resources :experiences
-  resources :users, only: [:new, :create, :destroy, :show, :index]
+  resources :users do
+    resources :concerts, only: [:new, :create, :index]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
